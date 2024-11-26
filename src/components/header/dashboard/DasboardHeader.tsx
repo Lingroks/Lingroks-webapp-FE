0e91480b0002f5f0f735f9dc23af60d1ef7d645c
inno-cent <ekweozorinnocent@gmail.com>
2024-11-26 20:15:23 +0100

    feat : header udate

--- src/components/header/dashboard/DasboardHeader.tsx
+++ src/components/header/dashboard/DasboardHeader.tsx
@@ -11,62 +11,63 @@ const Header = () => {
   };
 
   return (
-    <div className={styles.header}>
-      {/* Logo */}
-      <div className={styles.logo}>
-        <Link href="/">
-          <Image
-            src="/Lingroks.svg"
-            alt="Logo"
-            width={105}
-            height={100}
-            className=""
+    <>
+      <div className={styles.header}>
+        {/* Logo */}
+        <div className={styles.logo}>
+          <Link href="/">
+            <Image
+              src="/Lingroks.svg"
+              alt="Logo"
+              width={105}
+              height={100}
+              className=""
+            />
+          </Link>
+        </div>
+
+        {/* Search Input */}
+        <div className={`${styles.search} hidden md:flex`}>
+          <input
+            type="text"
+            placeholder="Search by name or service"
+            className={styles.search__input}
           />
-        </Link>
-      </div>
-
-      {/* Search Input */}
-      <div className={`${styles.search} hidden md:flex`}>
-        <input
-          type="text"
-          placeholder="Search by name or service"
-          className={styles.search__input}
-        />
-      </div>
-
-      {/* User Section */}
-      <div className={styles.user} onClick={toggleDropdown}>
-        <div className={styles.user__icon}>
-          <img src="/user.svg" alt="Logo" className="" />
-        </div>
-        <span className={styles.user__name}>Username</span>
-        <div className={styles.user__icon}>
-          <img src="/down.svg" alt="down" className="" />
         </div>
 
-        {/* Dropdown */}
-        {dropdownOpen && (
-          <div className={styles.user__dropdown}>
-            <ul>
-              <li>Personal Info</li>
-              <li>Security Changes</li>
-              <li>Payment</li>
-              <li>Terms and Privacy</li>
-              <li className="logout">Log Out</li>
-            </ul>
+        {/* User Section */}
+        <div className={styles.user} onClick={toggleDropdown}>
+          <div className={styles.user__icon}>
+            <img src="/user.svg" alt="Logo" className="" />
           </div>
-        )}
-      </div>
+          <span className={styles.user__name}>Username</span>
+          <div className={styles.user__icon}>
+            <img src="/down.svg" alt="down" className="" />
+          </div>
 
+          {/* Dropdown */}
+          {dropdownOpen && (
+            <div className={styles.user__dropdown}>
+              <ul>
+                <li>Personal Info</li>
+                <li>Security Changes</li>
+                <li>Payment</li>
+                <li>Terms and Privacy</li>
+                <li className="logout">Log Out</li>
+              </ul>
+            </div>
+          )}
+        </div>
+      </div>
       {/* Mobile View */}
-      {/* <div className={` ${styles["mobile-search"]}`}>
+      <div className={` ${styles['mobile-search']}`}>
         <input
           type="text"
           placeholder="Search by name or service"
-          className={styles["mobile-search__input"]}
+          className={styles['mobile-search__input']}
         />
-      </div> */}
-    </div>
+      </div>
+    </>
   );
 };
 
