69575825e6d586181ca09ba0b350c60f2c7095c1
inno-cent <ekweozorinnocent@gmail.com>
2024-12-11 13:07:52 +0100

    feat : dashboard update

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -1,12 +1,25 @@
 // Example: Importing and using in Dashboard.tsx
-import DashboardHeader from "../../components/header/dashboard/DasboardHeader";
+import DashboardHeader from '../../components/header/dashboard/DasboardHeader';
+import style from '../../assets/scss/Dashboard.module.scss';
 
 const Dashboard = () => {
-    return (
-        <div>
-            <DashboardHeader />
+  return (
+    <div>
+      <DashboardHeader />
+      <div className={style.dashboard__main__content}>
+        <div className={style.content__inner}>
+          <div className={style.headtext__wrap}>
+            <h3>
+              Hi, John
+              <span className={style.text__newline}>
+                What would you like to do today ?
+              </span>
+            </h3>
+          </div>
         </div>
-    );
-}
+      </div>
+    </div>
+  );
+};
 
 export default Dashboard;
