a55fc45fcd30ead9cb916c8876f8193e70b45719
inno-cent <ekweozorinnocent@gmail.com>
2024-12-13 21:45:54 +0100

    fixes

--- src/components/button/AuthButton.tsx
+++ src/components/button/AuthButton.tsx
@@ -1,3 +1,25 @@
+// import React from "react";
+
+// interface AuthButtonProps {
+//   icon: string;
+//   text?: string;
+//   className?: string;
+//   alt?: string;
+// }
+
+// const AuthButton: React.FC<AuthButtonProps> = ({icon, text = '', className = '', alt = ''}) => {
+//   return(
+//     <button className={`${className} py-2 px-4 rounded-[60px]`}>
+//         <div className='flex items-center justify-center'>
+//             <img src={icon} alt={alt} className='mr-2 w-[20px]' />
+//             <span className="ml-1 block text-[1rem] sm:text-[.7rem]">{text}</span>
+//         </div>
+//     </button>
+//   )
+// }
+
+// export default AuthButton;
+
 import React from "react";
 
 interface AuthButtonProps {
@@ -5,17 +27,24 @@ interface AuthButtonProps {
   text?: string;
   className?: string;
   alt?: string;
+  type?: "button" | "submit" | "reset"; // Explicitly defining the type
 }
 
-const AuthButton: React.FC<AuthButtonProps> = ({icon, text = '', className = '', alt = ''}) => {
-  return(
-    <button className={`${className} py-2 px-4 rounded-[60px]`}>
-        <div className='flex items-center justify-center'>
-            <img src={icon} alt={alt} className='mr-2 w-[20px]' />
-            <span className="ml-1 block text-[1rem] sm:text-[.7rem]">{text}</span>
-        </div>
+const AuthButton: React.FC<AuthButtonProps> = ({
+  icon,
+  text = '',
+  className = '',
+  alt = '',
+  type = "button", // Defaulting to "button" if not provided
+}) => {
+  return (
+    <button type={type} className={`${className} py-2 px-4 rounded-[60px]`}>
+      <div className="flex items-center justify-center">
+        <img src={icon} alt={alt} className="mr-2 w-[20px]" />
+        <span className="ml-1 block text-[1rem] sm:text-[.7rem]">{text}</span>
+      </div>
     </button>
-  )
-}
+  );
+};
 
 export default AuthButton;
