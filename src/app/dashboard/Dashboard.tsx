9c13f6068ea49ff34a60ce0eba8e9a00978dc19f
inno-cent <ekweozorinnocent@gmail.com>
2024-12-14 14:14:32 +0100

    feat : input component

--- src/app/dashboard/Dashboard.tsx
+++ src/app/dashboard/Dashboard.tsx
@@ -4,28 +4,6 @@ import DashboardHeader from '../../components/header/dashboard/DasboardHeader';
 import style from '../../assets/scss/Dashboard.module.scss';
 
 const Dashboard = () => {
-  // Dropdown states for Button 1 and Button 2
-  const [selectedOption1, setSelectedOption1] = useState('Translate');
-  const [dropdownOpen1, setDropdownOpen1] = useState(false);
-
-  const [selectedOption2, setSelectedOption2] = useState({
-    text: 'ENG',
-    icon: <EnglandFlagIcon />,
-  });
-  const [dropdownOpen2, setDropdownOpen2] = useState(false);
-
-  const button1Options = [
-    { text: 'Translate', icon: <TranslateIcon /> },
-    { text: 'Audio', icon: <AudioIcon /> },
-    { text: 'Summary', icon: <SummaryIcon /> },
-    { text: 'Insight', icon: <InsightIcon /> },
-  ];
-
-  const button2Options = [
-    { text: 'ENG', icon: <EnglandFlagIcon /> },
-    { text: 'ENG', icon: <UsaFlagIcon /> },
-  ];
-
   const cardData = [
     {
       headText: 'Lorem ipsum dolor sit amet consectetur.',
