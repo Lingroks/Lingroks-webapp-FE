72e6425f185e1aebc8b65b631d364b0a514a608b
inno-cent <ekweozorinnocent@gmail.com>
2025-01-07 19:37:27 +0100

    feat : translate style update

--- src/app/dashboard/translatepage/page.tsx
+++ src/app/dashboard/translatepage/page.tsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import Breadcrumb from '../../../components/breadcrumb/breadcrumb';
 import DashboardHeader from '../../../components/header/dashboard/DasboardHeader';
+import style from "../../../assets/scss/pages/translate.module.scss"
 
 const TranslatePage = () => {
   return (
     <>
       <DashboardHeader />
-      <div>
+      <div className={style.translate__main__content}>
         <Breadcrumb
           title="Home | Loom"
           date="29 October 2024"
