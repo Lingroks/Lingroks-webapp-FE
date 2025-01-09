e26f3e8c89afe1135c4e23f207a61af5948467a3
inno-cent <ekweozorinnocent@gmail.com>
2025-01-09 23:12:44 +0100

    security changes ui fix

--- src/app/dashboard/securitychanges/page.tsx
+++ src/app/dashboard/securitychanges/page.tsx
@@ -15,34 +15,38 @@ const SecurityChanges = () => {
           {' '}
           <a className={style.back_link}>&lt; Back</a>
         </Link>
-        <div className={style.header}>Security Changes</div>
-        <div className={style.input_group}>
-          <label className={style.label}>Old Password</label>
-          <input
-            type="password"
-            className={style.input}
-            placeholder="johndoe@gmail.com"
-          />
-        </div>
-        <div className={style.input_group}>
-          <label className={style.label}>New Password</label>
-          <input
-            type="password"
-            className={style.input}
-            placeholder="johndoe@gmail.com"
-          />
-        </div>
-        <div className={style.input_group}>
-          <label className={style.label}>Confirm New Password</label>
-          <input
-            type="password"
-            className={style.input}
-            placeholder="johndoe@gmail.com"
-          />
-        </div>
-        <div className={style.button_group}>
-          <button className={`${style.button} ${style.save}`}>Save</button>
-          <button className={`${style.button} ${style.cancel}`}>Cancel</button>
+        <div className={style.container_main_content}>
+          <div className={style.header}>Security Changes</div>
+          <div className={style.input_group}>
+            <label className={style.label}>Old Password</label>
+            <input
+              type="password"
+              className={style.input}
+              placeholder="johndoe@gmail.com"
+            />
+          </div>
+          <div className={style.input_group}>
+            <label className={style.label}>New Password</label>
+            <input
+              type="password"
+              className={style.input}
+              placeholder="johndoe@gmail.com"
+            />
+          </div>
+          <div className={style.input_group}>
+            <label className={style.label}>Confirm New Password</label>
+            <input
+              type="password"
+              className={style.input}
+              placeholder="johndoe@gmail.com"
+            />
+          </div>
+          <div className={style.button_group}>
+            <button className={`${style.button} ${style.save}`}>Save</button>
+            <button className={`${style.button} ${style.cancel}`}>
+              Cancel
+            </button>
+          </div>
         </div>
       </div>
     </div>
