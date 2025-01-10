7f31511135d1f066723dc4144f9c8768a2e07e02
inno-cent <ekweozorinnocent@gmail.com>
2025-01-10 20:34:33 +0100

    utils

--- src/utils/axoisInstance.js
+++ src/utils/axoisInstance.js
@@ -1,9 +1,11 @@
-import axios from "axios";
+import axios from 'axios';
 
 const axiosInstance = axios.create({
-  baseURL: process.env.API_BASE_URL || "http://localhost:8000/v1",
-  timeout: 5000,
+  baseURL: process.env.API_BASE_URL || 'http://localhost:8000/v1',
+  headers: {
+    'Content-Type': 'application/json',
+  },
+  timeout: 8000,
 });
 
 export default axiosInstance;
-
