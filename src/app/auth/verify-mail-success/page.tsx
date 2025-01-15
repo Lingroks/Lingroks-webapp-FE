f93c6628c70d3b94726fcd1b45d6772dc8f95d34
inno-cent <ekweozorinnocent@gmail.com>
2025-01-15 04:30:44 +0100

    fix: veriy email success

--- src/app/auth/verify-mail-success/page.tsx
+++ src/app/auth/verify-mail-success/page.tsx
@@ -1,26 +1,37 @@
-import React from "react";
-import { useNavigate } from "react-router-dom";
+'use client'
+
+
+import React from 'react';
+// import { useNavigate } from "react-router-dom";
+import { useRouter } from 'next/navigation';
+
 import AuthLayout from '@/app/auth/AuthLayout';
 import AuthMainBtn from '@/components/button/AuthMainBtn';
+import ForgotPasswordPic from '../../auth/forgot-password/ForgotPasswordPic';
 
 const SuccessPage: React.FC = () => {
-  const navigate = useNavigate();
+  const router = useRouter();
 
   const handleRedirect = () => {
-    navigate("/auth/login");
+    router.push('/auth/login');
   };
 
   return (
     <AuthLayout>
-      <div className="w-full flex items-center justify-center flex-col max-w-[430px] my-0 mx-auto px-5 pb-5">
-        <div className="flex items-center justify-center mb-4">
+      <div className="w-full flex items-center justify-center flex-col max-w-[430px] my-0 mx-auto mt-20 px-5 pb-5">
+        {/* <div className="flex items-center justify-center mb-4">
           <img src="/success-icon.svg" alt="success" className="w-20 h-20" />
-        </div>
+        </div> */}
+        <ForgotPasswordPic 
+                     pic='/correct-success.svg'
+                     alt='reset successful'
+                    />
         <h1 className="text-black text-[2rem] font-inter-medium mb-2">
           Success!
         </h1>
         <p className="text-secondaryGrey mb-6 text-[1rem] text-center">
-          Your account has been successfully verified. You can now log in to access your account.
+          Your account has been successfully verified. You can now log in to
+          access your account.
         </p>
         <AuthMainBtn
           text="Go to Login"
