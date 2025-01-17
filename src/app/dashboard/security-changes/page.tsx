2a7a84d0ebd8abac878fa91e6a0dcda9902843cc
Tunde <iamjosephtunde@gmail.com>
2025-01-17 20:01:04 +0100

    add audio icons

--- src/app/dashboard/security-changes/page.tsx
+++ src/app/dashboard/security-changes/page.tsx
@@ -5,8 +5,25 @@ import React from 'react';
 import DashboardHeader from '@/components/header/dashboard/DasboardHeader';
 import style from '@/assets/scss/pages/Dashboard.module.scss';
 import Link from 'next/link';
+import {useState} from 'react';
 
 const SecurityChanges = () => {
+  const [oldPassword, setOldPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
+  const [confirmPassword, setConfirmPassword] = useState('');
+
+  const handleOldPasswordChange = (e) => {
+    setOldPassword(e.target.value);
+  }
+
+  const handleNewPasswordChange = (e) => {
+    setNewPassword(e.target.value);
+  }
+
+  const handleConfirmPasswordChange = (e) => {
+    setConfirmPassword(e.target.value);
+  }
+
   return (
     <div>
       <DashboardHeader />
