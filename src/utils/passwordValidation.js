6ad2eca54131c0e371a01ae0fa4bbe7ce0e98919
inno-cent <ekweozorinnocent@gmail.com>
2025-02-02 18:19:53 +0100

    fixes

--- src/utils/passwordValidation.js
+++ src/utils/passwordValidation.js
@@ -1,11 +1,3 @@
-// export const validatePassword = (password) => {
-//     const criteria = {
-//       hasUppercase: /[A-Z]/.test(password),
-//       hasNumber: /\d/.test(password),
-//       isLongEnough: password.length >= 8,
-//     };
-//     return criteria;
-//   };
   
   export  const validatePassword = (password) => {
     return {
