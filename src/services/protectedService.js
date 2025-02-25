5393907f2f136df725117469cb02292a189afaa3
inno-cent <ekweozorinnocent@gmail.com>
2025-02-25 22:16:27 +0100

    fix: protect

--- src/services/protectedService.js
+++ src/services/protectedService.js
@@ -1,24 +1,27 @@
 // services/protectedService.js
 
-import axios from "axios";
+// import axios from "axios";
 import axiosInstance from "@/utils/axoisInstance";
 import { toast } from "react-toastify";
 
 const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
 const PROTECTED_URL = `${BASE_URL}/users/protected`;
 
-
 const checkProtectedRoute = async () => {
   try {
     await axiosInstance.post(PROTECTED_URL, {}); // No need to manually set headers
     return true; // Proceed if successful
-  } catch (error) {
-    toast.error("Access denied. Please check your subscription or log in again.");
-    throw new Error("Access denied.");
+  } catch (error: any) {
+    const errorMessage =
+      error?.response?.data?.message || "Access denied. Please try again.";
+    
+    toast.error(errorMessage);
+    throw new Error(errorMessage);
   }
 };
 
 export default checkProtectedRoute;
+
 
 
 // import { toast } from "react-toastify";
