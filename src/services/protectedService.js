6a07e4446187eb8ad7d8971d67da76cac81c3ff8
inno-cent <ekweozorinnocent@gmail.com>
2025-02-28 21:35:25 +0100

    fix: protect

--- src/services/protectedService.js
+++ src/services/protectedService.js
@@ -23,31 +23,3 @@ export default checkProtectedRoute;
 
 
 
-// import { toast } from "react-toastify";
-
-// const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL;
-// const PROTECTED_URL = `${BASE_URL}/users/protected`;
-
-// const checkProtectedRoute = async () => {
-//   try {
-//     const response = await fetch(PROTECTED_URL, {
-//       method: "POST",
-//       headers: {
-//         "Content-Type": "application/json",
-//       },
-//       body: JSON.stringify({}),
-//     });
-
-//     if (!response.ok) {
-//       toast.error("Access denied. Please check your subscription or log in again.");
-//       throw new Error("Access denied.");
-//     }
-
-//     return true; // Proceed if successful
-//   } catch (error) {
-//     toast.error("Access denied. Please check your subscription or log in again.");
-//     throw error;
-//   }
-// };
-
-// export default checkProtectedRoute;
